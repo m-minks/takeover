b7225e926850ff6334a05cc5965a1f99e0768dcc
joober <livinobernardinojoaopedro@gmail.com>
2026-05-08 21:25:00 -0300

    Fix Render port

--- server.js
+++ server.js
@@ -30,6 +30,8 @@ io.on('connection', (socket) => {
   });
 });
 
-server.listen(3000, () => {
-  console.log('Servidor online em http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`Servidor online na porta ${PORT}`);
 });
